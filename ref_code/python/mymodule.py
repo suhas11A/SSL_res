a590d08a664f98ddab94232faa2f5186d72514be
suhas <suhas1alladaboina@gmail.com>
2025-04-25 13:23:37 +0000

    finalized cs104 docx

--- ref_code/python/mymodule.py
+++
@@ -1,6 +0,0 @@
-# mymodule.py - This is the custom module
-
-# Function with default parameter
-def greet_with_default(name="Guest"):
-    """Function with a default parameter."""
-    print(f"Hello, {name}!")
